fe2047b84de66a4bd59d819e1b4571d969429896
Andrey Byelogurov <byelogurov@gmail.com>
2013-05-21 13:43:08 -0700

    Random stuff
    k

--- src/glStuffApp.cpp
+++ src/glStuffApp.cpp
@@ -1,5 +1,11 @@
-#include "cinder/app/AppNative.h"
-#include "cinder/gl/gl.h"
+#include "cinder\app\AppNative.h"
+#include "cinder\gl\gl.h"
+#include "cinder\gl\GlslProg.h"
+#include "cinder\gl\Vbo.h"
+#include "cinder\MayaCamUI.h"
+
+#define VERTx 20
+#define VERTy 20
 
 using namespace ci;
 using namespace ci::app;
@@ -10,27 +16,105 @@ public:
 	void setup();
 	void prepareSettings(Settings *settings);
 	void mouseDown(MouseEvent event);	
+	void mouseDrag(MouseEvent);
 	void update();
 	void draw();
+
+	gl::GlslProg shader;
+	gl::VboMeshRef mVbo;
+	MayaCamUI myCamera;
+
+	GLuint model,proj,view;
 };
 
+void glStuffApp::mouseDrag(MouseEvent event) {
+	myCamera.mouseDrag( event.getPos(), event.isLeftDown(), event.isMiddleDown(), event.isRightDown() );
+}
+
 void glStuffApp::prepareSettings(Settings *settings) {
-	settings->setWindowSize(800, 600);
-	settings->setFrameRate(60.0f);
+	//settings->setWindowSize(800, 600);
+	//settings->setFrameRate(60.0f);
 }
 
 void glStuffApp::setup() {
+
+	CameraPersp cam;
+	cam.setPerspective(75.0f, getWindowAspectRatio(), 5.0f, 2000.0f);
+	cam.setEyePoint(Vec3f(500, 0,0));
+	cam.setCenterOfInterestPoint(Vec3f::zero());
+	myCamera.setCurrentCam(cam);
+
+	try {
+		shader = gl::GlslProg(loadAsset("vert_shader.glsl"), loadAsset("frag_shader.glsl"));
+	} catch(gl::GlslProgCompileExc shaderError) {
+		console() << "Shader Compile error:" << endl;
+		console() << shaderError.what() << endl;
+	}
+
+		// setup the parameters of the Vbo
+	int totalVertices = VERTx*VERTy;
+	int totalQuads = VERTx*VERTy;
+
+	gl::VboMesh::Layout layout;
+	layout.setStaticIndices();
+	layout.setDynamicPositions();
+	layout.setStaticColorsRGB();
+	mVbo = gl::VboMesh::create( totalVertices, totalQuads * 4, layout, GL_QUADS );
+	
+	// buffer our static data - the texcoords and the indices
+	vector<uint32_t> indices;
+	vector<Color> texCoords;
+	int lol = 0;
+	for( int x = 0; x < VERTx; ++x ) {
+		for( int y = 0; y < VERTy; ++y, lol++ ) {
+			// create a quad for each vertex, except for along the bottom and right edges
+			if( ( x + 1 < VERTx ) && ( y + 1 < VERTy ) ) {
+				indices.push_back( (x+0) * VERTy + (y+0));
+				indices.push_back( (x+1) * VERTy + (y+0));
+				indices.push_back( (x+1) * VERTy + (y+1));
+				indices.push_back( (x+0) * VERTy + (y+1));
+			}
+			// the texture coordinates are mapped to [0,1.0)
+			texCoords.push_back(hsvToRGB(Vec3f((float)lol/totalVertices,1.0,1.0)));
+
+		}
+	}
+	
+	mVbo->bufferIndices(indices);
+	mVbo->bufferColorsRGB(texCoords);
+
 }
 
 void glStuffApp::mouseDown(MouseEvent event) {
 }
 
 void glStuffApp::update() {
+
+	gl::VboMesh::VertexIter iter = mVbo->mapVertexBuffer();
+	for( int x = 0; x < VERTx; ++x ) {
+		for( int y = 0; y < VERTy; ++y ) {
+			iter.setPosition(Vec3d(x,y,0));
+			++iter;
+		}
+	}
 }
 
 void glStuffApp::draw() {
 	gl::clear(Color(0, 0, 0)); 
+	//gl::pushMatrices();
+	gl::setMatrices(myCamera.getCamera());
+	gl::enableWireframe();
+	shader.uniform("model", myCamera.getCamera().getModelViewMatrix());
+	shader.uniform("projection", myCamera.getCamera().getProjectionMatrix());
+	//shader.uniform("view", myCamera.getCamera().get);
 
+	shader.bind();
+	
+	//gl::drawSolidCircle(Vec2f::zero(), 1);
+	gl::draw(mVbo);
+	shader.unbind();
+	gl::disableWireframe();
+	gl::popMatrices();
 }
 
 CINDER_APP_NATIVE(glStuffApp, RendererGl)
